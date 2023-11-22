0a02ad5c3848f413bd73bc4a8ee26fd51744d516
Paweł Jacewicz <pjacewicz474@gmail.com>
2023-11-22 08:38:58 +0100

    Add project files.

---
+++ InvoiceManager/Models/Domains/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Services.Description;
+
+namespace InvoiceManager.Models.Domains
+{
+    public class Product
+    {
+        public Product()
+        {
+            InvoicePositions = new Collection<InvoicePosition>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+
+        public ICollection<InvoicePosition> InvoicePositions { get; set; }
+    }
+}
---
+++ InvoiceManager/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using InvoiceManager.Models.Domains;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceManager.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} musi mieć co najmniej {2} znaków.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("Password", ErrorMessage = "Hasło i hasło potwierdzające nie są zgodne.")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Display(Name = "Nazwa")]
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+}
